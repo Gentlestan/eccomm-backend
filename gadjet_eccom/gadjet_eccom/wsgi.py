13f40f026933ac82826216f4429f5a63b9d119e9
Ohazulike Stanley <sohazulike@gmail.com>
2026-02-10 07:07:15 +0100

    updated wsgi

--- gadjet_eccom/gadjet_eccom/wsgi.py
+++ gadjet_eccom/gadjet_eccom/wsgi.py
@@ -2,15 +2,15 @@
 WSGI config for gadjet_eccom project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+# Add the repo root (eccomm-backend) to Python path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gadjet_eccom.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
